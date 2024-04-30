3abefbce245e93ee2e0017520ed229e744bff97d
Marcos Fernández <marcos.fersu@gmail.com>
2024-04-30 00:47:49 -0300

    add local data

---
+++ src/libs/data.ts
@@ -0,0 +1,87 @@
+export interface IAccordionData {
+  id: number;
+  title: string;
+  preTitle: string;
+  content: string[];
+}
+
+export const AccordionData: IAccordionData[] = [
+  {
+    id: 1,
+    title: "Marketing Médico",
+    preTitle: "MÓDULO I",
+    content: [
+      "Marketing y su evolución",
+      "Personal Branding",
+      "¿Cómo diferenciarnos en un entorno cada vez más competitivo?",
+      "Marketing Digital",
+      "Pasos para planear y ejecutar una estrategia digital",
+      "Principales errores en el uso de redes sociales",
+    ],
+  },
+  {
+    id: 2,
+    title: "La Experiencia del Paciente",
+    preTitle: "MÓDULO II",
+    content: [
+      "Marketing y su evolución",
+      "Personal Branding",
+      "¿Cómo diferenciarnos en un entorno cada vez más competitivo?",
+      "Marketing Digital",
+      "Pasos para planear y ejecutar una estrategia digital",
+      "Principales errores en el uso de redes sociales",
+    ],
+  },
+  {
+    id: 3,
+    title: "¿Cómo aumentar mis Ventas?",
+    preTitle: "MÓDULO III",
+    content: [
+      "Marketing y su evolución",
+      "Personal Branding",
+      "¿Cómo diferenciarnos en un entorno cada vez más competitivo?",
+      "Marketing Digital",
+      "Pasos para planear y ejecutar una estrategia digital",
+      "Principales errores en el uso de redes sociales",
+    ],
+  },
+  {
+    id: 4,
+    title: "Liderazgo & Equipos de Alto Rendimiento",
+    preTitle: "MÓDULO IV",
+    content: [
+      "Marketing y su evolución",
+      "Personal Branding",
+      "¿Cómo diferenciarnos en un entorno cada vez más competitivo?",
+      "Marketing Digital",
+      "Pasos para planear y ejecutar una estrategia digital",
+      "Principales errores en el uso de redes sociales",
+    ],
+  },
+  {
+    id: 5,
+    title: "Management de médico a empresario",
+    preTitle: "MÓDULO V",
+    content: [
+      "Marketing y su evolución",
+      "Personal Branding",
+      "¿Cómo diferenciarnos en un entorno cada vez más competitivo?",
+      "Marketing Digital",
+      "Pasos para planear y ejecutar una estrategia digital",
+      "Principales errores en el uso de redes sociales",
+    ],
+  },
+  {
+    id: 6,
+    title: "Productividad",
+    preTitle: "MÓDULO VI",
+    content: [
+      "Marketing y su evolución",
+      "Personal Branding",
+      "¿Cómo diferenciarnos en un entorno cada vez más competitivo?",
+      "Marketing Digital",
+      "Pasos para planear y ejecutar una estrategia digital",
+      "Principales errores en el uso de redes sociales",
+    ],
+  },
+];
