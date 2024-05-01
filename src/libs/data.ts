ad708cc97c81d22cc95d7b6b5647f3f735c47e87
Marcos Fernández <marcos.fersu@gmail.com>
2024-05-01 14:38:44 -0300

    Update all local data

--- src/libs/data.ts
+++ src/libs/data.ts
@@ -1,91 +1,192 @@
-import { m } from "framer-motion";
+export const HeroData = {
+  paragraph: {
+    head: {
+      first: "Practice Growth",
+      second: "Bootcamp",
+    },
+    subhead: "Llevá tu consultorio al siguiente nivel",
+    text: "El programa busca desarrollar nuevas habilidades para gerenciar su práctica y llevarla a otro nivel. Se enfoca en optimizar recursos, maximizar la rentabilidad y mejorar la experiencia del paciente para lograrlo",
+    btn: "Quiero inscribirme!",
+  },
+  image: {
+    main: "./main-img.webp",
+    main2: "./main-img-2.webp",
+  },
+};
 
-export type IProgramData = string;
-
-export const ProgramData: IProgramData[] = [
-  "Acceso a las grabaciones de las clases.",
-  "Material de apoyo con el resumen de cada clase.",
-  "Guías paso a paso, plantillas y workbooks para implementar los conceptos teóricos en su consultorio.",
-  "Scripts para que adapte y entregue a su staff.",
-  "Acceso a una sesión del Health Experience Mastermind.",
+export const InfoShortData = [
+  {
+    prehead: "Inicio y duración",
+    head: "13 de Mayo al 24 de Junio 12hs.",
+    icon: "clock",
+  },
+  {
+    prehead: "MODALIDAD",
+    head: "Online sincrónica",
+    icon: "desktop",
+  },
+  {
+    prehead: "DÍAS Y HORARIOS",
+    head: "Lunes de 19:30 a 21:30hs.",
+    icon: "calendar",
+  },
 ];
 
-export interface IAccordionData {
-  id: number;
-  title: string;
-  preTitle: string;
-  content: string[];
-}
+export const ImagFullOne = {
+  img: "./img-section.webp",
+  alt: "Trabajando con mi laptop",
+};
+export const ImagFullTwo = {
+  img: "./main-img-3.webp",
+  alt: "Paz Martorell",
+};
 
-export const AccordionData: IAccordionData[] = [
-  {
-    id: 1,
-    title: "Marketing Médico",
-    preTitle: "MÓDULO I",
-    content: [
-      "Marketing y su evolución",
-      "Personal Branding",
-      "¿Cómo diferenciarnos en un entorno cada vez más competitivo?",
-      "Marketing Digital",
-      "Pasos para planear y ejecutar una estrategia digital",
-      "Principales errores en el uso de redes sociales",
-    ],
+export const FundamentalsData = {
+  head: "Fundamentos del programa",
+  subhead:
+    "Muchos profesionales de la salud son excelentes y sin embargo no obtienen los resultados económicos que esperan.",
+  paragraph: {
+    one: "En un entorno cada vez más complejo, competitivo y en constante cambio, suele llegar un momento en el que se encuentran frente a grandes desafíos en áreas como Liderazgo, Marketing, Administración, Finanzas, Ventas y Operaciones, y muchas veces se sienten solos y que no están preparados para enfrentarlos.",
+    two: "No importa el momento de la carrera en el que se encuentre, en este programa encontrará propuestas innovadoras y disruptivas para enfrentar estos desafíos.",
+    three:
+      "Buscamos crear líderes que puedan asumir los desafíos que la salud hoy plantea.",
   },
-  {
-    id: 2,
-    title: "La Experiencia del Paciente",
-    preTitle: "MÓDULO II",
-    content: [
-      "¿Cómo eligen los pacientes?",
-      "Patient journey",
-      "¿Cómo conducir una consulta exitosa?",
-      "Negociación con el paciente",
-      "Fidelización de pacientes",
-    ],
+};
+
+export const GoalData = {
+  head: "Objetivos",
+  paragraph: {
+    one: "El objetivo es ayudar a profesionales de la salud a alcanzar el éxito, cualquiera sea la definición de éxito que tengan.",
+    two: "Para algunos puede ser acceder a más pacientes, para otros generar la misma facturación trabajando menos y para otros facturar más sin que ello implique más horas de trabajo.",
+    three:
+      "El programa busca desarrollar nuevas habilidades para gerenciar su práctica y llevarla a otro nivel. Se enfoca en optimizar recursos, maximizar la rentabilidad y mejorar la experiencia del paciente para lograrlo.",
   },
-  {
-    id: 3,
-    title: "¿Cómo aumentar mis Ventas?",
-    preTitle: "MÓDULO III",
-    content: [
-      "La venta como proceso",
-      "Diferentes etapas de la venta",
-      "Venta tradicional Vs. Venta profesional",
-      "Protocolos de venta",
-      "Neuroventas",
-    ],
+  image: {
+    img: "./doctor.webp",
+    alt: "Objetivos",
   },
-  {
-    id: 4,
-    title: "Liderazgo & Equipos de Alto Rendimiento",
-    preTitle: "MÓDULO IV",
-    content: [
-      "El rol del líder",
-      "Habilidades de coaching",
-      "Pasos para la incorporación de personal",
-      "Motivación e incentivos",
-    ],
+};
+
+export const ProgramListData = {
+  head: {
+    first: "El Programa",
+    second: "incluye:",
   },
-  {
-    id: 5,
-    title: "Management de médico a empresario",
-    preTitle: "MÓDULO V",
-    content: [
-      "Diferentes roles del manager",
-      "Información y métricas para la toma de decisiones",
-      "Gestión en base a objetivos",
-      "Evaluación de resultados y de comportamientos",
-      "La información como activo estratégico",
-      "Cómo lograr comportamientos a través de otros. Delegación efectiva",
-    ],
+  list: [
+    "Acceso a las grabaciones de las clases.",
+    "Material de apoyo con el resumen de cada clase.",
+    "Guías paso a paso, plantillas y workbooks para implementar los conceptos teóricos en su consultorio.",
+    "Scripts para que adapte y entregue a su staff.",
+    "Acceso a una sesión del Health Experience Mastermind.",
+  ],
+  btn: "Quiero inscribirme!",
+};
+
+export const PerfiltData = {
+  head: "Perfil",
+  text: "Dirigido a Profesionales de la salud de cualquier especialidad que:",
+  list: [
+    "Quieren generar un crecimiento económico y no saben cómo lograrlo.",
+    "Necesitan desarrollar nuevas habilidades para alcanzar sus objetivos.",
+    "Sienten que la mera intuición no es suficiente para la toma de decisiones.",
+    "Son conscientes de que el mundo cambió, que tienen que cambiar para ser competitivos pero no saben por dónde empezar.",
+    "Están abiertos a aprender, a ampliar su mirada, a cambiar su mindset y a escuchar nuevas propuestas e ideas.",
+    "Están dando los primeros pasos en su carrera pero quieren aplanar la curva de aprendizaje y evitar el método de prueba y error para lograr sus objetivos.",
+  ],
+};
+
+export const StudyPlanData = {
+  head: "Plan de estudio",
+  accordion: [
+    {
+      id: 1,
+      title: "Marketing Médico",
+      preTitle: "MÓDULO I",
+      content: [
+        "Marketing y su evolución",
+        "Personal Branding",
+        "¿Cómo diferenciarnos en un entorno cada vez más competitivo?",
+        "Marketing Digital",
+        "Pasos para planear y ejecutar una estrategia digital",
+        "Principales errores en el uso de redes sociales",
+      ],
+    },
+    {
+      id: 2,
+      title: "La Experiencia del Paciente",
+      preTitle: "MÓDULO II",
+      content: [
+        "¿Cómo eligen los pacientes?",
+        "Patient journey",
+        "¿Cómo conducir una consulta exitosa?",
+        "Negociación con el paciente",
+        "Fidelización de pacientes",
+      ],
+    },
+    {
+      id: 3,
+      title: "¿Cómo aumentar mis Ventas?",
+      preTitle: "MÓDULO III",
+      content: [
+        "La venta como proceso",
+        "Diferentes etapas de la venta",
+        "Venta tradicional Vs. Venta profesional",
+        "Protocolos de venta",
+        "Neuroventas",
+      ],
+    },
+    {
+      id: 4,
+      title: "Liderazgo & Equipos de Alto Rendimiento",
+      preTitle: "MÓDULO IV",
+      content: [
+        "El rol del líder",
+        "Habilidades de coaching",
+        "Pasos para la incorporación de personal",
+        "Motivación e incentivos",
+      ],
+    },
+    {
+      id: 5,
+      title: "Management de médico a empresario",
+      preTitle: "MÓDULO V",
+      content: [
+        "Diferentes roles del manager",
+        "Información y métricas para la toma de decisiones",
+        "Gestión en base a objetivos",
+        "Evaluación de resultados y de comportamientos",
+        "La información como activo estratégico",
+        "Cómo lograr comportamientos a través de otros. Delegación efectiva",
+      ],
+    },
+    {
+      id: 6,
+      title: "Productividad",
+      preTitle: "MÓDULO VI",
+      content: [
+        "Gestión del Tiempo",
+        "Estrategias para aumentar su productividad",
+      ],
+    },
+  ],
+  btn: "Quiero inscribirme!",
+};
+
+export const AboutData = {
+  prehead: "Directora",
+  head: "Paz Martorell",
+  paragraph: {
+    one: "Especialista en Marketing Médico, Patient Experience y Healthcare Management.",
+    two: "Directora del MicroMaster de Liderazgo & Satisfacción de Pacientes de la UCA.",
+    three:
+      "Fundadora de MedAdviser, consultora que hace más de 10 años acompaña a médicos para que transformen la forma en la que prestan sus servicios, para que operen negocios más rentables, brinden una mejor experiencia a sus pacientes y disfruten más de su profesión.",
+    four: "Lic. en Administración de Empresas y Contadora Pública (UCA). Postgraduate Diploma in Digital Business & Digital Marketing (Columbia Business School). Coach Ontológico (Newfield Network).",
+    five: "Más de 10 años de experiencia en el mundo corporativo en empresas como Arcor, L Oreal y Allergan.",
+    six: "Speaker en Congresos, Sociedades Médicas, Hospitales y reuniones científicas de reconocidos Laboratorios.",
   },
-  {
-    id: 6,
-    title: "Productividad",
-    preTitle: "MÓDULO VI",
-    content: [
-      "Gestión del Tiempo",
-      "Estrategias para aumentar su productividad",
-    ],
+  btn: "Contactar",
+  image: {
+    img: "/main-img-4.webp",
+    alt: "Paz Martorell - foto perfil",
   },
-];
+};
